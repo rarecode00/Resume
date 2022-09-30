0f8b51e2662b46d0adcbee0a5632da9b7cb62985
Krishna <ks9192174414@gmail.com>
2022-09-30 20:55:52 +0530

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Navigations => SCROLL DOWN
-
+alert("Please Open it in Laptop For better view :)");
 var current = 0 , target = 0;
 
 const onClick = function() {
@@ -7,15 +7,15 @@ const onClick = function() {
     if(this.id == 'home-nav'){
         target = 0;
     }else if(this.id == 'about-nav'){
-        target = 50;
+        target = 120;
     }else if(this.id == 'skill-nav'){
-        target = 660;
+        target = 920;
     }else if(this.id == 'education-nav'){
-        target = 1000;
+        target = 1580;
     }else if(this.id == 'portfolio-nav'){
-        target = 1700;
+        target = 2900;
     }else if(this.id == 'contact-nav'){
-        target = 2800;
+        target = 3400;
     }
 
 
@@ -38,6 +38,23 @@ const onClick = function() {
   document.getElementById('portfolio-nav').onclick = onClick;
   document.getElementById('contact-nav').onclick = onClick;
 
-  // SKILLS-SECTION ANIMATION
 
-document.getE
+const Name = document.getElementById('contacter-name');
+
+// SKILLS-SECTION ANIMATION
+
+var list_of_bars = document.querySelectorAll('.skill-progress');
+function set_width_to_zero()
+{
+   alert("one");
+}
+
+//Submit-button
+
+const button = document.getElementById('form-button');
+
+button.addEventListener('click' , function(){
+    //if(Name.textContent.length > 0){
+        if(Name.value) alert(`Hi ${Name.value} Hope You Like it !! If you have any type of query feel free to mail or whatsapp me i can reply you ASAP , my contact information is given in page :)`);
+    //}
+})
